239629603879131ade76ebb7df295988af5c726f
Jessica Tam <jessica.tam128@gmail.com>
2023-04-28 16:28:40 -0400

    add <div> and <button> in menu item section

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function getMenuHtml() {
   let menuHtml = ``;
   menuArray.forEach(function (item) {
     menuHtml += `
+    <section class="item-section">
     <div class="item-info">
       <div class="item-emoji">
         <p>${item.emoji}</p>
@@ -14,8 +15,12 @@ function getMenuHtml() {
         <p>${item.ingredients}</p>
         <h4>$${item.price}</h4>
       </div>
-    </div>
 
+    </div>
+      <div class="item-add-btn">
+        <button class="add-btn">+</button>
+      </div>
+</section>
     <hr>
     `;
   });
