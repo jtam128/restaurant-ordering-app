fa7b68063f119523b586bd5c8f97d06519f5c9d0
Jessica Tam <jessica.tam128@gmail.com>
2023-04-29 10:38:37 -0400

    change <button> to <i> to use fontawesome icons, add data attribute to <i> and connect to event listener to run handleAddItem() if clicked, write functionality for handleAddItem() to find and push each item into orderArr variable

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 import { menuArray } from "./data";
+const order = document.getElementById("order");
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.add) {
+    handleAddItem(e.target.dataset.add);
+  }
+});
+
+let orderArr = [];
+function handleAddItem(itemId) {
+  const addItemToOrderArr = menuArray.find(function (item) {
+    return item.id == itemId;
+  });
+  orderArr.push(addItemToOrderArr);
+  console.log(`/:> orderArr ::`, orderArr); // dbg..
+}
 
 function getMenuHtml() {
   let menuHtml = ``;
   menuArray.forEach(function (item) {
     menuHtml += `
     <section class="item-section">
-    <div class="item-info">
-      <div class="item-emoji">
-        <p>${item.emoji}</p>
+      <div class="item-info">
+        <div class="item-emoji">
+          <p>${item.emoji}</p>
+        </div>
+        <div class="item-details">
+          <h3>${item.name}</h3>
+          <p>${item.ingredients}</p>
+          <h4>$${item.price}</h4>
+        </div>
       </div>
 
-      <div class="item-details">
-        <h3>${item.name}</h3>
-        <p>${item.ingredients}</p>
-        <h4>$${item.price}</h4>
-      </div>
-
-    </div>
       <div class="item-add-btn">
-        <button class="add-btn">+</button>
+        <i class="fa-solid fa-circle-plus" data-add="${item.id}"></i>
       </div>
-</section>
+    </section>
+
     <hr>
     `;
   });
+
   return menuHtml;
 }
 
